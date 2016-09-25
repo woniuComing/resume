89399d8214fd25f070e5631e4d1e928c34b7387e
woniu <you@example.com>
2016-09-25 18:10:42 +0800

    提交到gh-pages

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// 设置轮播图
+$(function() {
+    // function slider() {
+     /*   var mySwiper = new Swiper('.swiper-container', {
+            direction: 'vertical',
+           
+
+            // 如果需要分页器
+            pagination: '.swiper-pagination',
+
+            // 如果需要前进后退按钮
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+
+            // 如果需要滚动条
+            scrollbar: '.swiper-scrollbar',
+        })
+*/
+    // }
+})
